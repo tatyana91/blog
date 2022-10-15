6954420db65d9fe559a5a1b957c2e75eb3afa8f1
tatyana <yata-2012@mail.ru>
2022-10-15 22:05:34 +0300

    Заменила проверку селектора на @selected. Убрала лишний use в модели постов.

--- resources/views/components/controls/select.blade.php
+++ resources/views/components/controls/select.blade.php
@@ -1,10 +1,9 @@
 <label for="{{ $id }}" class="form-label">{{ $label }}</label>
 <select class="form-select @error($name) is-invalid @enderror"
         aria-label="{{ $label }}" name="{{ $name }}" id="{{ $id }}">
-    <option value="" readonly>-</option>
+    <option value="" disabled @selected($value == '')>-</option>
     @foreach($options as $optionId => $optionTitle)
-        <option value="{{ $optionId }}"
-            {{ $value == $optionId ? 'selected' : '' }}>{{ $optionTitle }}</option>
+        <option value="{{ $optionId }}" @selected($value == $optionId)>{{ $optionTitle }}</option>
     @endforeach
 </select>
 @error($name) <div class="invalid-feedback">{{ $message }}</div> @enderror
