4733bac7849503e1cbf1560618c8be94f656e150
tatyana <yata-2012@mail.ru>
2022-10-15 22:33:34 +0300

    Правки миграции на добавление внешнего ключа постам. У категорий добавлен вывод постов.

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,4 +4,22 @@
     <hr>
     <div>{{ $category->description }}</div>
     <em>{{ $category->created_at }}</em>
+
+    <div>Posts:</div>
+    @forelse($category->posts as $post)
+        <div>{{ $post->title }}</div>
+        <em>{{ $post->created_at }}</em><br>
+        <div class="btnsBlock">
+            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">show</a>
+            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">edit</a>
+            <form method="post" action="{{ route('posts.destroy', $post->id) }}">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger">delete</button>
+            </form>
+        </div>
+        <hr>
+    @empty
+        <div>This category has no posts</div>
+    @endforelse
 </x-layouts.base>
